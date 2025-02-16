c527b9e912c92b6201183719e13c918a464ac444
gpego1 <gpego786@gmail.com>
2025-02-16 17:27:23 -0300

    Servico de automacao funcionando

---
+++ auto.py
@@ -0,0 +1,19 @@
+import pyautogui as pa
+import time
+import pyperclip
+pa.PAUSE = 1
+
+pa.press('win')
+pa.write("edge")
+pa.press('ENTER')
+pa.write("youtube.com")
+pa.press('ENTER')
+time.sleep(4)
+pa.click(x=716, y=151)
+pyperclip.copy("Kendrick Lamar")
+pa.hotkey('ctrl','v')
+pa.press('enter')
+time.sleep(4)
+pa.click(x=642, y=712)
+pa.click(x=826, y=968)
+pa.click()
